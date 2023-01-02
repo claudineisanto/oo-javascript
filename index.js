438f2d3264e01524d3b7d5d4f72ab544ad9bc626
Claudinei <claudinhoalves1984@gmail.com>
2023-01-02 10:30:03 -0300

    Demonstração do de como o  polimorfismo funciona

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+"use strict"
 import User from "./User.js";
 import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
-const novoUser = new User('Marina', 'm@m.com', '2021-01-01');
-console.log(novoUser.exibeInfos());
+//const novoUser = new User('Marina', 'm@m.com', '2021-01-01');
+//const novoAdmin = new Admin('Leticia', 'l@l.com', '2021-01-01')
+//console.log(novoUser.nome);
+
+//console.log(novoAdmin.nome)
+
+//novoAdmin.nome = ''
+//console.log(novoNome.nome)
+
+const novoDocente = new Docente('Luan', 'l@l.com', '2021-08-09');
+console.log(novoDocente.exibeInfos())// Aqui esta um ex: do polimorfismo!
+
 
 
